36784f4072baa5c8c28e6f54971bf1c695d3bd76
Kevin Geisler <geislekj@rose-hulman.edu>
2012-01-19 18:41:40 -0500

    Adding GenerationCodspike

---
+++ GenerationCodespike/Generation Codespike/src/Liza/LizaWorld.java
@@ -0,0 +1,13 @@
+package Liza;
+
+import org.bukkit.World;
+
+/**
+ * @author geislekj
+ *
+ *  LizaWorld is the Liza interface representation of
+ *  the Bukkit world interface
+ */
+public interface LizaWorld extends World{
+
+}
