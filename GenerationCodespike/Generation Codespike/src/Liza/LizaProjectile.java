38cfc97bf749b14f8f0488e7394a4aeb123a1bb1
Kevin Geisler <geislekj@rose-hulman.edu>
2012-01-31 13:56:40 -0500

    added the LizaProjectile and LizaCraftProjectile

---
+++ GenerationCodespike/Generation Codespike/src/Liza/LizaProjectile.java
@@ -0,0 +1,14 @@
+package Liza;
+
+import org.bukkit.entity.Projectile;
+
+
+/**
+ * @author geislekj
+ * 
+ *  LizaProjectile is the Liza interface representation of
+ *  the Bukkit Projectile interface
+ */
+public interface LizaProjectile extends Projectile{
+
+}
