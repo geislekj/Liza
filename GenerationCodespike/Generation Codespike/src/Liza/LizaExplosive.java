3fb430c29103310fb5e29c5eb8584185e6c22440
Kevin Geisler <geislekj@rose-hulman.edu>
2012-02-01 12:06:22 -0500

    added Explosive and Weather

---
+++ GenerationCodespike/Generation Codespike/src/Liza/LizaExplosive.java
@@ -0,0 +1,13 @@
+package Liza;
+
+import org.bukkit.entity.Explosive;
+
+/**
+ * @author geislekj
+ * 
+ *  LizaExplosive is the Liza interface representation of
+ *  the Bukkit Explosive interface
+ */
+public interface LizaExplosive extends Explosive{
+
+}
