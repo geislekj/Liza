e8161f5afb432d7acd88691ee2cdb1f595019dd5
Kevin Geisler <geislekj@rose-hulman.edu>
2012-02-01 12:28:35 -0500

    added Item and FallingSand

---
+++ GenerationCodespike/Generation Codespike/src/Liza/LizaFallingSand.java
@@ -0,0 +1,14 @@
+package Liza;
+
+import org.bukkit.entity.FallingSand;
+
+
+/**
+ * @author geislekj
+ * 
+ *  LizaFallingSand the Liza interface representation of
+ *  the Bukkit FallingSand interface
+ */
+public interface LizaFallingSand extends FallingSand{
+
+}
---
+++ GenerationCodespike/Generation Codespike/src/Liza/LizaItem.java
@@ -0,0 +1,14 @@
+package Liza;
+
+import org.bukkit.entity.Item;
+
+
+/**
+ * @author geislekj
+ * 
+ *  LizaItem is the Liza interface representation of
+ *  the Bukkit Item interface
+ */
+public interface LizaItem extends Item{
+
+}
