5a0a3ee61c0537be34799b514630578ff91e6ea9
Brian Collins <collinbc@rose-hulman.edu>
2012-02-05 20:58:53 -0500

    updated creature

--- GenerationCodespike/Generation Codespike/src/LizaCraft/Entity/LizaCraftCreature.java
+++ GenerationCodespike/Generation Codespike/src/LizaCraft/Entity/LizaCraftCreature.java
@@ -25,6 +25,9 @@ import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.util.Vector;
 
 import Liza.LizaCreature;
+import LizaCraft.LizaCraftServer;
+import LizaCraft.LizaCraftWorld;
+import LizaCraft.Block.LizaCraftBlock;
 
 /**
  * @author geislekj
@@ -47,22 +50,22 @@ public class LizaCraftCreature implements LizaCreature{
 
 	@Override
 	public LivingEntity getTarget() {
-		return this.creature.getTarget();
+		return new LizaCraftLivingEntity(this.creature.getTarget());
 	}
 
 	@Override
-	public void setTarget(LivingEntity arg0) {
-		this.creature.setTarget(arg0);
+	public void setTarget(LivingEntity target) {
+		this.creature.setTarget(target);
 	}
 
 	@Override
-	public void damage(int arg0) {
-		this.creature.damage(arg0);
+	public void damage(int amount) {
+		this.creature.damage(amount);
 	}
 
 	@Override
-	public void damage(int arg0, Entity arg1) {
-		this.creature.damage(arg0, arg1);
+	public void damage(int amount, Entity source) {
+		this.creature.damage(amount, source);
 	}
 
 	@Override
@@ -71,10 +74,13 @@ public class LizaCraftCreature implements LizaCreature{
 	}
 
 	@Override
-	public double getEyeHeight(boolean arg0) {
-		return this.creature.getEyeHeight(arg0); 
+	public double getEyeHeight(boolean ignoreSneaking) {
+		return this.creature.getEyeHeight(ignoreSneaking);
 	}
 
+	/**
+	 * TODO: Change once LizaCraftLocation is implemented.
+	 */
 	@Override
 	public Location getEyeLocation() {
 		return this.creature.getEyeLocation();
@@ -85,6 +91,9 @@ public class LizaCraftCreature implements LizaCreature{
 		return this.creature.getHealth();
 	}
 
+	/**
+	 * TODO: Change once LizaCraftLocation is implemented.
+	 */
 	@Override
 	public Player getKiller() {
 		return this.creature.getKiller();
@@ -96,13 +105,26 @@ public class LizaCraftCreature implements LizaCreature{
 	}
 
 	@Override
-	public List<Block> getLastTwoTargetBlocks(HashSet<Byte> arg0, int arg1) {
-		return this.creature.getLastTwoTargetBlocks(arg0, arg1); 
+	public List<Block> getLastTwoTargetBlocks(HashSet<Byte> transparent,
+			int maxDistance) {
+		List<Block> bl = this.creature.getLastTwoTargetBlocks(transparent,
+				maxDistance);
+
+		for (Block b : bl) {
+			b = new LizaCraftBlock(b);
+		}
+		return bl;
 	}
 
 	@Override
-	public List<Block> getLineOfSight(HashSet<Byte> arg0, int arg1) {
-		return this.creature.getLineOfSight(arg0, arg1); 
+	public List<Block> getLineOfSight(HashSet<Byte> transparent, int maxDistance) {
+		List<Block> bl = this.creature.getLastTwoTargetBlocks(transparent,
+				maxDistance);
+
+		for (Block b : bl) {
+			b = new LizaCraftBlock(b);
+		}
+		return bl;
 	}
 
 	@Override
@@ -131,13 +153,14 @@ public class LizaCraftCreature implements LizaCreature{
 	}
 
 	@Override
-	public Block getTargetBlock(HashSet<Byte> arg0, int arg1) {
-		return this.creature.getTargetBlock(arg0, arg1); 
+	public Block getTargetBlock(HashSet<Byte> transparent, int maxDistance) {
+		return new LizaCraftBlock(this.creature.getTargetBlock(transparent,
+				maxDistance));
 	}
 
 	@Override
 	public Vehicle getVehicle() {
-		return this.creature.getVehicle();
+		return new LizaCraftVehicle(this.creature.getVehicle());
 	}
 
 	@Override
@@ -145,56 +168,68 @@ public class LizaCraftCreature implements LizaCreature{
 		return this.creature.isInsideVehicle();
 	}
 
+	/**
+	 * This method performs an action and returns a value.
+	 */
 	@Override
 	public boolean leaveVehicle() {
 		return this.creature.leaveVehicle();
 	}
 
 	@Override
-	public void setHealth(int arg0) {
-		this.creature.setHealth(arg0);
+	public void setHealth(int health) {
+		this.creature.setHealth(health);
 	}
 
 	@Override
-	public void setLastDamage(int arg0) {
-		this.creature.setLastDamage(arg0);
+	public void setLastDamage(int damage) {
+		this.creature.setLastDamage(damage);
 	}
 
 	@Override
-	public void setMaximumAir(int arg0) {
-		this.creature.setMaximumAir(arg0);
+	public void setMaximumAir(int ticks) {
+		this.creature.setMaximumAir(ticks);
 	}
 
 	@Override
-	public void setMaximumNoDamageTicks(int arg0) {
-		this.creature.setMaximumNoDamageTicks(arg0);
+	public void setMaximumNoDamageTicks(int ticks) {
+		this.creature.setMaximumNoDamageTicks(ticks);
 	}
 
 	@Override
-	public void setNoDamageTicks(int arg0) {
-		this.creature.setNoDamageTicks(arg0);
+	public void setNoDamageTicks(int ticks) {
+		this.creature.setNoDamageTicks(ticks);
 	}
 
 	@Override
-	public void setRemainingAir(int arg0) {
-		this.creature.setRemainingAir(arg0);
+	public void setRemainingAir(int ticks) {
+		this.creature.setRemainingAir(ticks);
 	}
 
 	@Override
 	public Arrow shootArrow() {
-		return this.creature.shootArrow();
+		return new LizaCraftArrow(this.creature.shootArrow());
 	}
 
+	/**
+	 * TODO: Change once LizaCraftLocation is implemented.
+	 */
 	@Override
 	public Egg throwEgg() {
 		return this.creature.throwEgg();
 	}
 
+	/**
+	 * TODO: Change once LizaCraftLocation is implemented.
+	 */
 	@Override
 	public Snowball throwSnowball() {
 		return this.creature.throwSnowball();
 	}
 
+	/**
+	 * This method performs an action and returns a value.
+	 */
 	@Override
 	public boolean eject() {
 		return this.creature.eject();
@@ -215,11 +250,17 @@ public class LizaCraftCreature implements LizaCreature{
 		return this.creature.getFireTicks();
 	}
 
+	/**
+	 * TODO: Review this method.
+	 */
 	@Override
 	public EntityDamageEvent getLastDamageCause() {
 		return this.creature.getLastDamageCause();
 	}
 
+	/**
+	 * TODO: Change once LizaCraftLocation is implemented.
+	 */
 	@Override
 	public Location getLocation() {
 		return this.creature.getLocation();
@@ -230,19 +271,35 @@ public class LizaCraftCreature implements LizaCreature{
 		return this.creature.getMaxFireTicks();
 	}
 
+	/**
+	 * @param x
+	 *            Size of the box along x axis
+	 * @param y
+	 *            Size of the box along y axis
+	 * @param z
+	 *            Size of the box along z axis
+	 */
 	@Override
-	public List<Entity> getNearbyEntities(double arg0, double arg1, double arg2) {
-		return this.creature.getNearbyEntities(arg0, arg1, arg2); 
+	public List<Entity> getNearbyEntities(double x, double y, double z) {
+		List<Entity> el = this.creature.getNearbyEntities(x, y, z);
+
+		for (Entity e : el) {
+			e = new LizaCraftEntity(e);
+		}
+		return el;
 	}
 
 	@Override
 	public Entity getPassenger() {
-		return this.creature.getPassenger();
+		return new LizaCraftEntity(this.creature.getPassenger());
 	}
 
+	/**
+	 * TODO: Review this method.
+	 */
 	@Override
 	public Server getServer() {
-		return this.creature.getServer();
+		return new LizaCraftServer(this.creature.getServer());
 	}
 
 	@Override
@@ -255,6 +312,9 @@ public class LizaCraftCreature implements LizaCreature{
 		return this.creature.getUniqueId();
 	}
 
+	/**
+	 * TODO: Change if LizaCraftVector is implemented.
+	 */
 	@Override
 	public Vector getVelocity() {
 		return this.creature.getVelocity();
@@ -262,7 +322,7 @@ public class LizaCraftCreature implements LizaCreature{
 
 	@Override
 	public World getWorld() {
-		return this.creature.getWorld();
+		return new LizaCraftWorld(this.creature.getWorld());
 	}
 
 	@Override
@@ -276,8 +336,8 @@ public class LizaCraftCreature implements LizaCreature{
 	}
 
 	@Override
-	public void playEffect(EntityEffect arg0) {
-		this.creature.playEffect(arg0);
+	public void playEffect(EntityEffect type) {
+		this.creature.playEffect(type);
 	}
 
 	@Override
@@ -286,53 +346,68 @@ public class LizaCraftCreature implements LizaCreature{
 	}
 
 	@Override
-	public void setFallDistance(float arg0) {
-		this.creature.setFallDistance(arg0);
+	public void setFallDistance(float distance) {
+		this.creature.setFallDistance(distance);
 	}
 
 	@Override
-	public void setFireTicks(int arg0) {
-		this.creature.setFireTicks(arg0);
+	public void setFireTicks(int ticks) {
+		this.creature.setFireTicks(ticks);
 	}
 
 	@Override
-	public void setLastDamageCause(EntityDamageEvent arg0) {
-		this.creature.setLastDamageCause(arg0);
+	public void setLastDamageCause(EntityDamageEvent event) {
+		this.creature.setLastDamageCause(event);
+	}
+
+	/**
+	 * This method performs an action and returns a value.
+	 */
+	@Override
+	public boolean setPassenger(Entity passenger) {
+		return this.creature.setPassenger(passenger);
 	}
 
 	@Override
-	public boolean setPassenger(Entity arg0) {
-		return this.creature.setPassenger(arg0); 
+	public void setTicksLived(int ticks) {
+		this.creature.setTicksLived(ticks);
 	}
 
 	@Override
-	public void setTicksLived(int arg0) {
-		this.creature.setTicksLived(arg0);
+	public void setVelocity(Vector vel) {
+		this.creature.setVelocity(vel);
 	}
 
+	/**
+	 * This method performs an action and returns a value.
+	 */
 	@Override
-	public void setVelocity(Vector arg0) {
-		this.creature.setVelocity(arg0);
+	public boolean teleport(Location location) {
+		return this.creature.teleport(location);
 	}
 
+	/**
+	 * This method performs an action and returns a value.
+	 */
 	@Override
-	public boolean teleport(Location arg0) {
-		return this.creature.teleport(arg0); 
+	public boolean teleport(Entity destination) {
+		return this.creature.teleport(destination);
 	}
 
+	/**
+	 * This method performs an action and returns a value.
+	 */
 	@Override
-	public boolean teleport(Entity arg0) {
-		return this.creature.teleport(arg0);
+	public boolean teleport(Location location, TeleportCause cause) {
+		return this.creature.teleport(location, cause);
 	}
 
+	/**
+	 * This method performs an action and returns a value.
+	 */
 	@Override
-	public boolean teleport(Location arg0, TeleportCause arg1) {
-		return this.creature.teleport(arg0, arg1); 
+	public boolean teleport(Entity destination, TeleportCause cause) {
+		return this.creature.teleport(destination, cause);
 	}
 
-	@Override
-	public boolean teleport(Entity arg0, TeleportCause arg1) {
-		return this.creature.teleport(arg0, arg1); 
-	}	
-	
 }
