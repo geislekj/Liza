ff6e04b0da9f3e8e4e505448849e70105321ce6e
Sam Kim <kimsj@rose-hulman.edu>
2012-05-08 14:18:07 -0400

    Clean up code

--- Liza0.2/src/LizaCraft/LizaCraftTestModule.java
+++ Liza0.2/src/LizaCraft/LizaCraftTestModule.java
@@ -41,24 +41,12 @@ import org.bukkit.plugin.UnknownDependencyException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import Liza.LizaServer;
+import LizaCraft.Events.EventEnabler;
 import LizaCraft.Events.LizaPlugin;
 
-public class LizaCraftTestModule {
-	private static final String MINECRAFT_SERVER_THREAD_NAME = "Server thread";
-	private static final String MINECRAFT_SERVER_FIELD_NAME = "a";
-
-	private static final String PLUGIN_MANAGER_LIST_NAME = "plugins";
-	private static final String PLUGIN_MANAGER_FILE_ASSOCIATION_MAP = "fileAssociations";
-
-	private static final String PLUGIN_ENABLED_FIELD_NAME = "isEnabled";
-	private static final String PLUGIN_SERVER_FIELD_NAME = "server";
-	private static final String PLUGIN_LOADER_FIELD_NAME = "loader";
-	private static final String PLUGIN_DESCRIPTION_FIELD_NAME = "description";
-	private static final String PLUGIN_LOOKUP_FIELD_NAME = "lookupNames";
-
-	private static final String PLUGIN_EXTENSION = ".jar";
-	
+public class LizaCraftTestModule {	
 	private ServerGrabber serverGrabber = new ServerGrabber();
+	private EventEnabler eventEnabler = new EventEnabler();
 
 	private LizaServerThread serverThread;
 	private LizaServer server;
@@ -87,104 +75,12 @@ public class LizaCraftTestModule {
 	}
 
 	public void disableEvents() {
-		// TODO Auto-generated method stub
-
+		this.server.getPluginManager().disablePlugin(this.eventListener);
 	}
 
 	@SuppressWarnings("unchecked")
 	public void enableEvents() {
-		// TODO: Use something other than Field f a bunch of times
-
-		// Step 1: Find the appropriate PluginLoader
-		Class<? extends PluginManager> pluginManager = ((SimplePluginManager) this.server
-				.getPluginManager()).getClass();
-		Field f;
-		PluginLoader pluginLoader = null;
-		try {
-			f = pluginManager
-					.getDeclaredField(PLUGIN_MANAGER_FILE_ASSOCIATION_MAP);
-			f.setAccessible(true);
-			Map<Pattern, PluginLoader> fileAssociations = ((HashMap) f
-					.get(this.server.getPluginManager()));
-
-			for (Pattern p : fileAssociations.keySet()) {
-				Matcher match = p.matcher(PLUGIN_EXTENSION);
-				if (match.find()) {
-					pluginLoader = fileAssociations.get(p);
-				}
-			}
-			f.setAccessible(false);
-		} catch (Exception e) {
-			// TODO: Proper exception handling
-			e.printStackTrace();
-		}
-
-		// Step 2: Tell the PluginLoader to load the plugin
-		// Step 2.a: Create PluginDescriptionFile object
-		PluginDescriptionFile desc = new PluginDescriptionFile(
-				LizaPlugin.PLUGIN_NAME, null, null);
-
-		// Step 2.b: Call initialize on the plugin
-		Class<? extends JavaPlugin> plugin = this.eventListener.getClass();
-		try {
-			f = plugin.getSuperclass().getDeclaredField(
-					PLUGIN_ENABLED_FIELD_NAME);
-			f.setAccessible(true);
-			f.set(this.eventListener, true);
-			f.setAccessible(false);
-
-			f = plugin.getSuperclass().getDeclaredField(
-					PLUGIN_LOADER_FIELD_NAME);
-			f.setAccessible(true);
-			f.set(this.eventListener, pluginLoader);
-			f.setAccessible(false);
-
-			f = plugin.getSuperclass().getDeclaredField(
-					PLUGIN_SERVER_FIELD_NAME);
-			f.setAccessible(true);
-			f.set(this.eventListener, this.server);
-			f.setAccessible(false);
-
-			f = plugin.getSuperclass().getDeclaredField(
-					PLUGIN_DESCRIPTION_FIELD_NAME);
-			f.setAccessible(true);
-			f.set(this.eventListener, desc);
-			f.setAccessible(false);
-		} catch (Exception e) {
-			// TODO
-			e.printStackTrace();
-		}
-
-		// Step 3: Add to list of plugins
-		List<Plugin> plugins;
-		try {
-			f = pluginManager.getDeclaredField(PLUGIN_MANAGER_LIST_NAME);
-			f.setAccessible(true);
-			plugins = (List<Plugin>) f.get(this.server.getPluginManager());
-			plugins.add(this.eventListener);
-			f.set(this.server.getPluginManager(), plugins);
-			f.setAccessible(false);
-		} catch (Exception e) {
-			// TODO
-			e.printStackTrace();
-		}
-
-		// Step 4: Add to lookup table
-		Map<String, Plugin> lookupNames;
-		try {
-			f = pluginManager.getDeclaredField(PLUGIN_LOOKUP_FIELD_NAME);
-			f.setAccessible(true);
-			lookupNames = (HashMap<String, Plugin>) f.get(this.server
-					.getPluginManager());
-			lookupNames.put(LizaPlugin.PLUGIN_NAME, this.eventListener);
-			f.set(this.server.getPluginManager(), lookupNames);
-			f.setAccessible(false);
-		} catch (Exception e) {
-			// TODO
-			e.printStackTrace();
-		}
-
-		this.eventListener.onEnable();
+		eventEnabler.enableEvents(this.server, this.eventListener);
 	}
 
 	/**
@@ -225,6 +121,7 @@ public class LizaCraftTestModule {
 		} catch (InterruptedException e) {
 			
 		}
+		
 		this.server = new LizaCraftServer(this.serverGrabber.searchThreads());
 	}
 
@@ -243,14 +140,7 @@ public class LizaCraftTestModule {
 		try {
 			ret = this.server.getPluginManager().loadPlugin(file);
 			this.server.getPluginManager().enablePlugin(ret);
-		} catch (UnknownDependencyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidPluginException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidDescriptionException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
