6facc0f70dfd71293f3dd56703b5a699b3474ef1
unknown <williamb@WILLIAMB-1.rose-hulman.edu>
2012-05-09 17:10:08 -0400

    create Minecarts

---
+++ Liza0.2/src/LizaCraft/Entity/LizaCraftMinecart.java
@@ -0,0 +1,68 @@
+package LizaCraft.Entity;
+
+import org.bukkit.entity.Minecart;
+import org.bukkit.util.Vector;
+
+import Liza.LizaMinecart;
+
+public class LizaCraftMinecart extends LizaCraftVehicle implements LizaMinecart {
+
+	public LizaCraftMinecart(Minecart minecart){
+		super(minecart);
+	}
+	
+	public Minecart getBukkitHandle(){
+		return (Minecart)this.entity;
+	}
+
+	@Override
+	public int getDamage() {
+		return this.getBukkitHandle().getDamage();
+	}
+
+	@Override
+	public Vector getDerailedVelocityMod() {
+		return this.getBukkitHandle().getDerailedVelocityMod();
+	}
+
+	@Override
+	public Vector getFlyingVelocityMod() {
+		return this.getBukkitHandle().getFlyingVelocityMod();
+	}
+
+	@Override
+	public double getMaxSpeed() {
+		return this.getBukkitHandle().getMaxSpeed();
+	}
+
+	@Override
+	public boolean isSlowWhenEmpty() {
+		return this.getBukkitHandle().isSlowWhenEmpty();
+	}
+
+	@Override
+	public void setDamage(int damage) {
+		this.getBukkitHandle().setDamage(damage);
+	}
+
+	@Override
+	public void setDerailedVelocityMod(Vector velocity) {
+		this.getBukkitHandle().setDerailedVelocityMod(velocity);
+	}
+
+	@Override
+	public void setFlyingVelocityMod(Vector velocity) {
+		this.getBukkitHandle().setFlyingVelocityMod(velocity);
+	}
+
+	@Override
+	public void setMaxSpeed(double speed) {
+		this.getBukkitHandle().setMaxSpeed(speed);
+	}
+
+	@Override
+	public void setSlowWhenEmpty(boolean slow) {
+		this.getBukkitHandle().setSlowWhenEmpty(slow);
+	}
+	
+}
