003105938dbc011c45e35687d0b2629a81fd7330
Sam Kim <kimsj@rose-hulman.edu>
2012-05-09 17:29:50 -0400

    Update Pig

--- Liza0.2/src/Liza/LizaPig.java
+++ Liza0.2/src/Liza/LizaPig.java
@@ -13,12 +13,4 @@ import org.bukkit.entity.Pig;
  */
 public interface LizaPig extends Pig {
 
-	List<LizaBlock> getLastTwoTargetLizaBlocks(HashSet<Byte> transparent,
-			int maxDistance);
-
-	List<LizaBlock> getLineOfSightLiza(HashSet<Byte> transparent,
-			int maxDistance);
-
-	List<LizaEntity> getNearbyLizaEntities(double x, double y, double z);
-
 }
